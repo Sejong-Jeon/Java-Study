480bce34d6ae8c763dea3f24153e6ef5184b1ba9
Sejong-Jeon <sejong9193@gmail.com>
2019-05-03 00:47:51 +0900

    테스트추가

---
+++ Test/test.java
@@ -0,0 +1,18 @@
+
+/**
+ * test1
+ * 
+ * @author (전세종) 
+ * @version (2019-05-03)
+ */
+public class test
+{
+    public static void main(String args[]){
+        int a = 10;
+        int b = 20;
+        
+        System.out.println(a);
+        System.out.println(b);
+        
+    }
+}
